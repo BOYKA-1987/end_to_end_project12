53cd5d766431bcb39a7584e02fcd502f0487145e
“vipul <vipul1987more@gmail.com>
2024-08-24 00:59:12 +0530

    done

--- src/DimondPricePrediction/components/data_transformation.py
+++ src/DimondPricePrediction/components/data_transformation.py
@@ -102,13 +102,24 @@ class DataTransformation:
         
             
             input_feature_train_arr=preprocessing_obj.fit_transform(input_feature_train_df)
+            
             input_feature_test_arr=preprocessing_obj.transform(input_feature_test_df)
             
             logging.info("Applying preprocessing object on training and testing datasets.")
 
+            train_arr = np.c_[input_feature_train_arr, np.array(target_feature_train_df)]
+            test_arr = np.c_[input_feature_test_arr, np.array(target_feature_test_df)]
+
             save_object(
                 file_path=self.data_transformation_config.preprocessor_obj_file_path,
                 obj=preprocessing_obj
+            )
+
+            logging.info("preprocessing pickle file saved")
+            
+            return (
+                train_arr,
+                test_arr
             )
             
 
