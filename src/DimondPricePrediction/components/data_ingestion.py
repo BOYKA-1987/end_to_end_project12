4276c5503fe15f7055c909727c3861854054a85d
“vipul <vipul1987more@gmail.com>
2024-08-24 02:27:29 +0530

    output showed

--- src/DimondPricePrediction/components/data_ingestion.py
+++ src/DimondPricePrediction/components/data_ingestion.py
@@ -47,7 +47,13 @@ class DataIngestion:
             test_data.to_csv(self.ingestion_config.test_data_path,index=False)
 
             logging.info("data ingestion part complited..")
-
+            
+            return (
+                 
+                
+                self.ingestion_config.train_data_path,
+                self.ingestion_config.test_data_path
+            )
 
 
         except Exception as e:
